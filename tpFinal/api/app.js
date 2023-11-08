21b8b1732165e35b9588dfaeb797dcff38bb98b6
JoaquinMogno <joaquin.mogno@estudiantes.unahur.edu.ar>
2023-11-08 08:47:49 -0300

    Se aplica JWT

--- tpFinal/api/app.js
+++ tpFinal/api/app.js
@@ -1,5 +1,5 @@
 var createError = require('http-errors');
-var express = require('express');
+//var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -9,16 +9,78 @@ var comisionesRouter = require('./routes/comisions');
 var docentesRouter = require('./routes/docentes');
 var materiasRouter = require('./routes/materias');
 var periodo_lectivosRouter = require('./routes/periodo_lectivos');
+//var app = express();
 
-var app = express();
+//var jwt = require('jsonwebtoken');
+//var keys = require('./config/keys')
+const express = require('express');
+const app = express();
+const jwt = require('jsonwebtoken');
+const keys = require('./config/keys');
+app.set('key', keys.key);
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+
+
+app.post('/login', (req, res)=>{
+  if(req.body.usuario == 'admin'&& req.body.pass == '12345'){
+    const payload = {
+      check:true
+    };
+    const token = jwt.sign(payload, app.get('key'),{
+      expiresIn:'7d'
+    });
+    res.json({
+      message:'Autenticación exitosa!',
+      token: token
+    });
+  }else{
+    res.json({
+      message:'Usuario y/o contraseña incorrectas'
+    })
+  }
+});
+
+const verificacion = express.Router();
+
+verificacion.use((req, res, next)=>{
+  let token = req.headers['x-access-token'] || req.headers['authorization'];
+  //console.log(token);
+  if(!token){
+    res.status(401).send({
+      error:'Es necesario un token de autentificación'
+    })
+    return
+  }
+  if(token.startsWith('Bearer ')){
+    token = token.slice(7,token.length)
+    console.log(token)
+  }
+  if(token){
+    jwt.verify(token, app.get('key'), (error, decoded)=>{
+      if(error){
+        return res.json({
+          message:'El token no es valido'
+        });
+      }else{
+         req.decoded = decoded;
+         next();
+      }
+    })
+  }
+});
+
+app.get('/info', verificacion, (req, res)=>{
+  res.json('INFORMACION IMPORTANTE ENTREGADA');
+})
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
